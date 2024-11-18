22b2d6ca22adcadaf02c5eef0a74e2ba346a5ee0
yuki312 <define1394@gmail.com>
2024-11-18 19:52:16 +0900

    add failed test

--- flagfit-lint/src/test/java/tv/abema/flagfit/FlagTypeExpiryDateIllegalParamDetectorText.kt
+++ flagfit-lint/src/test/java/tv/abema/flagfit/FlagTypeExpiryDateIllegalParamDetectorText.kt
@@ -184,6 +184,40 @@ class FlagTypeExpiryDateIllegalParamDetectorText : LintDetectorTest() {
               )
               @FlagType.WorkInProgress(
                 owner = "Hoge Fuga",
+                expiryDate = "2023-12-10"
+              )
+              fun awesomeFeatureEnabled(): Boolean
+          }
+          """.trimIndent()
+        )
+      )
+      .issues(*issues.toTypedArray())
+      .allowMissingSdk()
+      .run()
+      .expectClean()
+  }
+
+  @Test
+  fun testExpiryDateNotDefinedExpireDateFormatNoWarning() {
+    lint()
+      .files(
+        stabBooleanFlag,
+        stabFlagType,
+        kotlin(
+          """
+          package foo
+          import tv.abema.flagfit.FlagType
+          import tv.abema.flagfit.annotation.BooleanFlag
+          import tv.abema.flagfit.FlagType.Companion.OWNER_NOT_DEFINED
+          import tv.abema.flagfit.FlagType.Companion.EXPIRY_DATE_INFINITE
+          
+          interface Example {
+              @BooleanFlag(
+                key = "new-awesome-feature",
+                defaultValue = false
+              )
+              @FlagType.WorkInProgress(
+                owner = "Hoge Fuga",
                 expiryDate = EXPIRY_DATE_NOT_DEFINED
               )
               fun awesomeFeatureEnabled(): Boolean
